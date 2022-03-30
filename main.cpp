b572ef45009a435bd4b18d92933bc6110b102328
карина сакович <102471349+7898h@users.noreply.github.com>
2022-03-30 17:58:42 +0000

    number of array elements

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
-}
+#include <iostream> 
+using namespace std; 
+int main() 
+{ setlocale(0,".1251"); 
+  int a[10],  max,  i,min ; double L ;
+cout<<"\nВведiть 10 цілих елементів масиву:\n"; 
+  for (int i=0; i<10; i++)  cin >> a[i]; 
+min=a[0]; max = a[0];
+for (i=0; i<10; i++)
+  if (a[i] > max) { max=a[i];}
+  for (i=0; i<10; i++)
+  if (a[i] < min) { min=a[i];}
+  
+  L=(min+max)/2;
+  cout<<"\nМаксимальне значення = " << max <<endl;
+  cout<<"Мінімальне значення = " << min <<endl;
+  cout<<"Cереднє значення = " << L <<endl;
+  system ("pause>>void"); 
+  return 0; 
+} 
